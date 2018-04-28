09f71ff99365f400ca878858b06f1fe283aae1cc
Keroks <adam.keroks@gmail.com>
2018-04-28 22:31:23 +0200

    Working MFRC522 module card present reading.

--- main.py
+++ main.py
@@ -3,23 +3,34 @@ Created on 24.03.2018
 
 @author: Adam Skorek
 '''
+import esp
+esp.osdebug(None)
 
 from machine import Pin, I2C, Timer, ADC
 import time
 import network
 import usocket as socket
 import ParserAT
-import ssd1306
+import mfrc522
+from os import uname
 
 led = Pin(2, Pin.OUT)
-ssid = 'TestSSID'
-password = 'TestPassword'
+ssid = 'Niepokoj_Hotel'
+password = 'N13p0k0j4c3'
 wlan = network.WLAN(network.STA_IF)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 addr = socket.getaddrinfo("192.168.1.40", 9999)[0][-1]
-adc = ADC(0)
-i2c = I2C(-1, Pin(5), Pin(4))
-display = ssd1306.SSD1306_I2C(64, 48, i2c)
+
+rotatedPrevious = False
+rotated = False
+skipOneRead = True
+
+if uname()[0] == 'WiPy':
+    rdr = mfrc522.MFRC522("GP14", "GP16", "GP15", "GP22", "GP17")
+elif uname()[0] == 'esp8266':
+    rdr = mfrc522.MFRC522(12, 15, 14, 0, 13)
+else:
+    raise RuntimeError("Unsupported platform")
 
 
 def do_connect():
@@ -54,14 +65,53 @@ def led_cmd(operand, params):
         return "WRONG OPERAND"
 
 
-def read_battery(t):
-    battery_nominal_voltage = 3.7
-    current_voltage = (adc.read() * battery_nominal_voltage) / 1024
-    print("Battery: {}V".format(current_voltage))
-    sock.sendto("+BAT={:1.2f}V\r\n".format(current_voltage).encode("utf-8"), addr)
-    display.fill(0)
-    display.text("Bat:{:1.2f}V".format(current_voltage), 0, 0)
-    display.show()
+def read_card(t):
+    (stat, tag_type) = rdr.request(rdr.REQIDL)
+
+    global rotated
+    global rotatedPrevious
+    global skipOneRead
+
+    if stat == rdr.OK:
+        # print('REQIDL returned OK!')
+        rotated = True
+        skipOneRead = True
+    else:
+        # print('REQIDL returned {}!'.format(stat))
+        if not skipOneRead:
+            rotated = False
+        skipOneRead = False
+
+    if rotated != rotatedPrevious:
+            if rotated:
+                print('+ROT=1')
+                sock.sendto(b"+ROT=1\r\n", addr)
+            else:
+                print('+ROT=0')
+                sock.sendto(b"+ROT=0\r\n", addr)
+            rotatedPrevious = rotated
+    # else:
+        # print('No change!')
+        # (stat, raw_uid) = rdr.anticoll()
+        #
+        # if stat == rdr.OK:
+        #
+        #     print("New card detected")
+        #     print("  - tag type: 0x%02x" % tag_type)
+        #     print("  - uid	 : 0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+        #     print("")
+        #
+        #     if rdr.select_tag(raw_uid) == rdr.OK:
+        #
+        #         key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+        #
+        #         if rdr.auth(rdr.AUTHENT1A, 8, key, raw_uid) == rdr.OK:
+        #             print("Address 8 data: %s" % rdr.read(8))
+        #             rdr.stop_crypto1()
+        #         else:
+        #             print("Authentication error")
+        #     else:
+        #         print("Failed to select tag")
 
 
 def main():
@@ -76,8 +126,8 @@ def main():
 
     timCon = Timer(-1)
     timCon.init(period=10000, mode=Timer.PERIODIC, callback=check_connection)
-    timBat = Timer(1)
-    timBat.init(period=5000, mode=Timer.PERIODIC, callback=read_battery)
+    timRfid = Timer(1)
+    timRfid.init(period=100, mode=Timer.PERIODIC, callback=read_card)
 
     while True:
         # print("Working...")
@@ -95,6 +145,7 @@ def main():
                     sock.sendto(rspStr.encode("utf-8"), addr)
             elif isinstance(response, str):
                 sock.sendto(response.encode("utf-8"), addr)
+
         time.sleep(1)
 
 
